ab347f235efbd4c36ff51fa4021ea8b4dbf8d96d
JacobDiaz7 <jaco.diazr@duocuc.cl>
2026-04-14 13:21:29 -0400

    Add files via upload

---
+++ for.py
@@ -0,0 +1,35 @@
+
+# for i in range(3):
+#     print("repeticion N", i+1)
+
+#
+
+# num=int(input("INGRESE UN NUMERO: "))
+# for i in range(num):
+#     print(i+1, "Hola usuario")
+
+
+# num=int(input("INGRESE UN NUMERO: "))
+# for i in range(10):
+#     print(num, "X", i, num*i )
+
+
+# num=int(input("INGRESE la cant de notas: "))
+# suma=0
+# for i in range (num):
+#     nota=float(input("Ingrese la nota: "))
+#     suma=suma+nota
+# prom=suma/num
+# print("el promedio es", prom)
+# if prom>=4:
+#     print("el alumno aprob")
+# else:
+#     print("el alumno reprobo")
+
+
+# nombre=input ("Ingrese su nombre")
+# cantLetra=0
+# for i in "nombre":
+#     print(i)
+#     cantLetra=cantLetra+1
+# print("La cantidad de caracteres es", cantLetra)
