c2aa55e291e0d2cee05a80722595b8a9c93411da
JacobDiaz7 <jaco.diazr@duocuc.cl>
2026-04-14 11:44:22 -0400

    primer commit

---
+++ prueba.py
@@ -0,0 +1,14 @@
+# print("INGRRESE SU NOMBRE")
+# nombre=input()
+# edad=int(input("INGRESE SU EDAD"))
+
+
+# print("Hola", nombre, "su edad en  5 años sera", edad+5)
+
+
+# print("ingrese un numero")
+# numero1=int(input())
+# print("ingrese su segundo numero")
+# numero2=int(input())
+
+# print("la suma de sus numeros es", numero1+numero2, "la reta es", numero1-numero2,)
