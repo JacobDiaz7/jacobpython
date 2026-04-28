8be91d01122658dab3278a83814c5d4d9d7d3c7b
JacobDiaz7 <jaco.diazr@duocuc.cl>
2026-04-28 13:24:21 -0400

    Add files via upload

---
+++ randomly.py
@@ -0,0 +1,62 @@
+# uso y ejemplo de random
+
+import random ; import time
+
+# num=random.randint(1,10)
+# print(num)
+
+# for i in range(num):
+#     print("Hola deny")
+
+
+# dado1=random.randint(1,6)
+# dado2=random.randint(1,6)
+
+# print(f"el dado 1 dio {dado1}")
+# print(f"el dado 2 dio {dado2}")
+
+# if dado1==dado2:
+#     print("se va a la carcel")
+# else:
+#     print("avance por favor")
+
+# numA=random.randint(1,100)
+
+# for i in range(5):
+#     num=int(input("Ingrese un numero: "))
+#     if num>numA:
+#         print("te pasaste")
+#     elif num<numA:
+#         print("el numero adivinar es mayor")
+#     elif num==numA:
+#         print("adivinaste el numero")
+#         break
+# if num!=numA:
+#     print(f"perdiste se acabaron tus intentos, el numero adivinar era {numA}")
+
+
+import random ; import time
+
+# golpe=random.randint(7,18)
+
+
+# name1=input("Ingrese el nombre del jugador 1")
+# name2=input("ingrese el nombre del jugador 2")
+# p1=100
+# p2=100
+
+# while p1>=0 and p2>=0:
+#     golpe=random.randint(7,18)
+#     print(f"el jugador 1 golpeo al jugador 2 le quito {golpe}")
+#     p2=p2-golpe
+#     time.sleep(2)
+#     golpe=random.randint(7,18)
+#     print(f"el jugador 2 golpeo al jugador 1 le quito {golpe}")
+#     p1=p1-golpe
+#     time.sleep(2)
+#     if p1==0:
+#         print("el jugador 2 gano")
+#     elif p2==0:
+#         print("el jugador 1 gano")
+    
+
